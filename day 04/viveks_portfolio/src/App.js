57308ec6712a5c7b805e819657d94fa596cd3030
Vivek Thakur <20509.vivek@gpranchi.org>
2023-10-18 21:23:36 +0530

    commit

--- day 04/viveks_portfolio/src/App.js
+++ day 04/viveks_portfolio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from './Components/Navbar/Navbar';
 import Section from './Components/Section/Section';
+import LanguagesTools from "./Components/LangTools/LanguagesTools";
 
 function App() {
   return (
     <>
       <Navbar />
       <Section />
+      <LanguagesTools/>
     </>
   );
 }
---
+++ day 04/viveks_portfolio/src/Components/LangTools/LanguagesTools.js
@@ -0,0 +1,12 @@
+import "./LanguagesTool.css";
+
+const LanguagesTools = () => {
+    return (
+        <div className="languages_tools">
+            This gonna be my languages_tools Section
+
+        </div>
+    );
+};
+
+export default LanguagesTools;
